d5324aabecfaf0079c34c946756a33b5ed5e4b59
Victoria Pleschko <victoria.pleschko@gmail.com>
2021-04-07 12:06:09 +0100

    adding more tools

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import nuke
 
 def setup_menu():
     nuke.menu('Nuke').addCommand('VP Tools/Basic Match Grade', create_basic_match_grade_setup)
+    nuke.menu('Nuke').addCommand('VP Tools/Basic Script', create_basic_script)
+    nuke.menu('Nuke').addCommand('VP Tools/Tidy Nodes', tidyNodes)
 
 
 def create_basic_match_grade_setup():
@@ -9,7 +11,28 @@ def create_basic_match_grade_setup():
     Grade1 = nuke.nodes.Grade(black_clamp=False)
     Grade2 = nuke.nodes.Grade(inputs=[Grade1], black_clamp=False)
     Grade3 = nuke.nodes.Grade(inputs=[Grade2])
-    
+
+def create_basic_script():
+    ReadNode = nuke.nodes.Constant(label='Read Node Placeholder', color=1)
+    Dot = nuke.nodes.Dot(inputs=[ReadNode])
+    nuke.nodes.Write(inputs=[Dot])
+
+def tidyNodes():
+    nodes = nuke.selectedNodes()    
+    amount = len(nodes)    
+    if amount == 0:    
+        return 
+
+    allX = sum([n.xpos()+n.screenWidth()/2 for n in nodes])  
+    allY = sum([n.ypos()+n.screenHeight()/2 for n in nodes]) 
+
+    centreX = allX / amount
+    centreY = allY / amount
+
+    for n in nodes:
+        n.setXpos(centreX + ( n.xpos() - centreX) * 1)
+        n.setYpos(centreY + ( n.ypos() - centreY) * 1)
+
     
 
 
